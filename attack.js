323c9133b66942da4ca564263238752e997d65dd
Pattem-Chaitanya <chaitanya.pattem@ftlabs.io>
2024-03-31 12:41:16 +0530

    saving without linting

--- attack.js
+++ attack.js
@@ -1,3 +1,5 @@
-const attack = (name, damage, isCritical) => `${name} dealt ${isCritical ? damage * 2 : damage} damage!`;
+const attack = (name, damage, isCritical) => {
+  return `${name} dealt ${isCritical ? damage * 2 : damage} damage!`;
+};
 
 module.exports = attack;
