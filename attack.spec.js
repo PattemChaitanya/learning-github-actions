80bed951d70009549f85fd62e2d871182569713c
Pattem-Chaitanya <chaitanya.pattem@ftlabs.io>
2024-03-31 17:16:15 +0530

    added jest testing library

---
+++ attack.spec.js
@@ -0,0 +1,7 @@
+const attack = require('./attack');
+
+test('should attack return', () => {
+  const result = attack('sony', 23);
+  // expect(result).toBe('sony dealt 23 damage!');
+  expect(result).toMatchSnapshot();
+});
